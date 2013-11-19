37b73c62e11dd261432dc1fd931224eef41d5568
Taco Verdo <taco@yoast.com>
2013-11-19 15:16:00 +0100

    Moved search to top of menu items (hamburger menu)

--- lib/functions/hamburger.php
+++ lib/functions/hamburger.php
@@ -6,6 +6,7 @@ function enqueue_hamburgermenu_scripts() {
 	wp_enqueue_script( 'modernizr', get_stylesheet_directory_uri() . '/lib/js/modernizr.js' );
 	wp_enqueue_script( 'hamburgermain', get_stylesheet_directory_uri() . '/lib/js/main.js', false, null, true );
 }
+
 add_action( 'wp_enqueue_scripts', 'enqueue_hamburgermenu_scripts' );
 
 /**
@@ -14,36 +15,43 @@ add_action( 'wp_enqueue_scripts', 'enqueue_hamburgermenu_scripts' );
 function enqueue_styles_hambugermenu() {
 	wp_enqueue_style( 'style-name', get_stylesheet_directory_uri() . '/assets/css/ROCM.css' );
 }
+
 add_action( 'wp_enqueue_scripts', 'enqueue_styles_hambugermenu' );
 
 function add_to_genesis_header() {
 	echo '<a class="nav-btn" id="nav-open-btn" href="#nav">&nbsp;</a>';
 }
+
 add_action( 'genesis_header', 'add_to_genesis_header' );
 
 function add_to_genesis_menu() {
 	echo '<a class="close-btn" id="nav-close-btn" href="#top">Close navigation</a>';
 }
+
 add_action( 'genesis_after_header', 'add_to_genesis_menu' );
 
-function add_search_to_wp_menu ( $items, $args ) {
-	if( 'primary' === $args -> theme_location ) {
-		$search = get_search_form(false);
-		$items .= '<li class="menu-item menu-item-search">';
-		$items .= $search;
-		$items .= '</li>';
+function add_search_to_wp_menu( $items, $args ) {
+	if ( 'primary' === $args->theme_location ) {
+		$search = get_search_form( false );
+		$add = '<li class="menu-item menu-item-search">';
+		$add .= $search;
+		$add .= '</li>';
+		$items = $add . $items;
 	}
 	return $items;
 }
-add_filter('wp_nav_menu_items','add_search_to_wp_menu',10,2);
+
+add_filter( 'wp_nav_menu_items', 'add_search_to_wp_menu', 10, 2 );
 
 function add_wrappers_open() {
 	echo '<div id="outer-wrap"><div id="inner-wrap">';
 }
+
 //add_action( 'genesis_before_header', 'add_wrappers_open' );
 function add_wrappers_close() {
 	echo '</div></div>';
 }
+
 //add_action( 'genesis_after_footer', 'add_wrappers_close' );
 
 function add_div_for_taco( $pre, $args ) {
@@ -60,4 +68,5 @@ function add_div_for_taco( $pre, $args ) {
 	else
 		return $changed_tag;
 }
-add_filter('genesis_markup_site-container', 'add_div_for_taco', 10, 2);
+
+add_filter( 'genesis_markup_site-container', 'add_div_for_taco', 10, 2 );
