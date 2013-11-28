174664e253ed479baa8e6edd5407b7efffbc2fff
Joost de Valk <joost@yoast.com>
2013-11-28 11:23:48 +0100

    Completely clean up functions.php Combine sidr and sticky menu JS code Add setup function for child theme which has all hooks for maintainability Added PHPdoc openers for all functions Cleaned up footer text function, replaced complete function with simple filter on the text. Changed namespace for i18n to yoast-theme. Added several @todo’s and @fixme’s for faulty code.

--- functions.php
+++ functions.php
@@ -1,103 +1,141 @@
 <?php
-//* Start the engine
-include_once( get_template_directory() . '/lib/init.php' );
 
-/** Load widgets from /lib/widgets/ */
-foreach ( glob( CHILD_DIR . "/lib/widgets/*-widget.php" ) as $file ) {
-	require_once $file;
+//* Child theme (do not remove)
+define( 'CHILD_THEME_NAME', 'Tailor Made' );
+define( 'CHILD_THEME_URL', 'http://yoast.com/wordpress/themes/tailor-made/' );
+define( 'CHILD_THEME_VERSION', '0.0.1' );
+
+add_action( 'genesis_setup', 'child_theme_setup', 15 );
+
+/**
+ *
+ */
+function child_theme_setup() {
+	// Used for defaults in for instance the banner widget.
+	define( 'YST_SIDEBAR_WIDTH', 261 );
+
+		//* Start the engine
+	include_once( get_template_directory() . '/lib/init.php' );
+
+	// Setup Theme Settings
+	// @todo Does this always have to be loaded or just in admin?
+	include_once( CHILD_DIR . '/lib/functions/child-theme-settings.php' );
+
+	/** Load widgets from /lib/widgets/ */
+	foreach ( glob( CHILD_DIR . "/lib/widgets/*-widget.php" ) as $file ) {
+		require_once $file;
+	}
+
+	//include_once( get_theme_root_uri() . '/theme001/lib/functions/yst-colourscheme-settings.php' );
+	require_once( 'lib/functions/yst-colourscheme-settings.php' );
+
+	//* Add HTML5 markup structure
+	add_theme_support( 'html5' );
+
+	//* Add viewport meta tag for mobile browsers
+	add_theme_support( 'genesis-responsive-viewport' );
+
+//* Add support for custom background
+	add_theme_support( 'custom-background' );
+
+//* Add support for 3-column footer widgets
+	add_theme_support( 'genesis-footer-widgets', 4 );
+
+	// Disable site layouts that must not be used
+	genesis_unregister_layout( 'content-sidebar-sidebar' );
+	genesis_unregister_layout( 'sidebar-sidebar-content' );
+	genesis_unregister_layout( 'sidebar-content-sidebar' );
+
+	/** Register widget areas */
+	genesis_register_sidebar( array(
+		'id'          => 'yoast-top-right',
+		'name'        => __( 'Search', 'yoast-theme' ),
+		'description' => __( 'Search widget area. Intended for search widget. Changes drastically on mobile.', 'yoast-theme' ),
+	) );
+
+	genesis_register_sidebar( array(
+		'id'          => 'yoast-after-header-1',
+		'name'        => __( 'After Header 1', 'yoast-theme' ),
+		'description' => __( 'After Header 1 widget area.', 'yoast-theme' ),
+	) );
+
+	genesis_register_sidebar( array(
+		'id'          => 'yoast-after-header-2',
+		'name'        => __( 'After Header 2', 'yoast-theme' ),
+		'description' => __( 'After Header 2 widget area.', 'yoast-theme' ),
+	) );
+
+	genesis_register_sidebar( array(
+		'id'          => 'yoast-after-header-3',
+		'name'        => __( 'After Header 3', 'yoast-theme' ),
+		'description' => __( 'After Header 3 widget area.', 'yoast-theme' ),
+	) );
+
+	genesis_register_sidebar( array(
+		'id'          => 'yoast-tagline-after-header',
+		'name'        => __( 'Tagline After Header', 'yoast-theme' ),
+		'description' => __( 'Tagline After Header widget area.', 'yoast-theme' ),
+	) );
+
+	// Activate blogroll widget
+	add_filter( 'pre_option_link_manager_enabled', '__return_true' );
+
+	add_action( 'wp_enqueue_scripts', 'yst_load_css_from_setting' );
+	add_action( 'wp_enqueue_scripts', 'enqueue_styles_basic' );
+	add_action( 'wp_enqueue_scripts', 'yst_add_google_fonts' );
+	add_action( 'wp_enqueue_scripts', 'yst_include_sidr' );
+
+	add_action( 'wp_head', 'yst_header_sidr_css' );
+
+	add_action( 'genesis_header', 'yst_mobile_nav' );
+
+	add_action( 'genesis_after_header', 'yst_after_header_genesis' );
+
+	//* Reposition the breadcrumbs
+	add_action( 'genesis_after_header', 'genesis_do_breadcrumbs' );
+	remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
+
+	add_action( 'genesis_header', 'yst_add_top_right_area' );
+
+	add_action( 'genesis_before_loop', 'yoast_term_archive_intro', 20 );
+
+	add_action( 'wp_footer', 'yst_activate_sidr_and_sticky_menu' );
+
+	// Add Read More Link to Excerpts
+	add_filter( 'excerpt_more', 'yst_get_read_more_link' );
+	add_filter( 'the_content_more_link', 'yst_get_read_more_link' );
+
+	add_filter( 'genesis_footer_creds_text', 'yst_footer_creds_text' );
+
+	add_filter( 'genesis_comment_list_args', 'yst_comments_gravatar' );
 }
 
-//include_once( get_theme_root_uri() . '/theme001/lib/functions/yst-colourscheme-settings.php' );
-require_once( 'lib/functions/yst-colourscheme-settings.php' );
-
-// Load the right CSS
+/**
+ *
+ * @fixme this uses a hardcoded theme path....
+ */
 function yst_load_css_from_setting() {
 	wp_enqueue_style( 'yst_custom_css', get_theme_root_uri() . "/theme001" . genesis_get_option( 'yst_colourscheme' ), array( 'google-font-quattrocento_sans', 'admin-bar', 'theme001' ) );
 }
 
-add_action( 'wp_enqueue_scripts', 'yst_load_css_from_setting' );
-
+/**
+ *
+ */
 function enqueue_styles_basic() {
 	wp_enqueue_style( 'style-basic', get_stylesheet_directory_uri() . '/assets/css/style.css' );
 }
 
-add_action( 'wp_enqueue_scripts', 'enqueue_styles_basic' );
-
-//* Child theme (do not remove)
-define( 'CHILD_THEME_NAME', 'Theme001' );
-define( 'CHILD_THEME_URL', 'http://yoast.com/' );
-define( 'CHILD_THEME_VERSION', '0.0.1' );
-
-//* Enqueue Google font
+/**
+ * Enqueue Google font
+ */
 function yst_add_google_fonts() {
 	wp_enqueue_style( 'google-font-quattrocento_sans', '//fonts.googleapis.com/css?family=Quattrocento+Sans:400,400italic,700,700italic);', array(), CHILD_THEME_VERSION );
 }
 
-add_action( 'wp_enqueue_scripts', 'yst_add_google_fonts' );
-
-//* Add HTML5 markup structure
-add_theme_support( 'html5' );
-
-//* Add viewport meta tag for mobile browsers
-add_theme_support( 'genesis-responsive-viewport' );
-
-//* Add support for custom background
-add_theme_support( 'custom-background' );
-
-//* Add support for 3-column footer widgets
-add_theme_support( 'genesis-footer-widgets', 4 );
-
-// Used for defaults in for instance the banner widget.
-define( 'YST_SIDEBAR_WIDTH', 261 );
-
-// Activate blogroll widget
-add_filter( 'pre_option_link_manager_enabled', '__return_true' );
-
-// Disable site layouts that must not be used
-genesis_unregister_layout( 'content-sidebar-sidebar' );
-genesis_unregister_layout( 'sidebar-sidebar-content' );
-genesis_unregister_layout( 'sidebar-content-sidebar' );
-
-// Setup Theme Settings
-include_once( CHILD_DIR . '/lib/functions/child-theme-settings.php' );
-
-/** Register widget areas */
-genesis_register_sidebar( array(
-	'id'          => 'yoast-top-right',
-	'name'        => __( 'Search', 'yoast' ),
-	'description' => __( 'Search widget area. Intended for search widget. Changes drastically on mobile.', 'yoast' ),
-) );
-
-genesis_register_sidebar( array(
-	'id'          => 'yoast-after-header-1',
-	'name'        => __( 'After Header 1', 'yoast' ),
-	'description' => __( 'After Header 1 widget area.', 'yoast' ),
-) );
-
-genesis_register_sidebar( array(
-	'id'          => 'yoast-after-header-2',
-	'name'        => __( 'After Header 2', 'yoast' ),
-	'description' => __( 'After Header 2 widget area.', 'yoast' ),
-) );
-
-genesis_register_sidebar( array(
-	'id'          => 'yoast-after-header-3',
-	'name'        => __( 'After Header 3', 'yoast' ),
-	'description' => __( 'After Header 3 widget area.', 'yoast' ),
-) );
-
-genesis_register_sidebar( array(
-	'id'          => 'yoast-tagline-after-header',
-	'name'        => __( 'Tagline After Header', 'yoast' ),
-	'description' => __( 'Tagline After Header widget area.', 'yoast' ),
-) );
-
-
 /**
- * Add yoast-after-header widget support for site. If widget not active, don't display
- *
+ * Add yst-after-header widget support for site. If widget not active, don't display
  */
-function yoast_after_header_genesis() {
+function yst_after_header_genesis() {
 
 	echo '<div id="yoast-after-header-container"><div class="wrap">';
 
@@ -129,12 +167,10 @@ function yoast_after_header_genesis() {
 	}
 }
 
-add_action( 'genesis_after_header', 'yoast_after_header_genesis' );
-
 /**
  * Add top-right widget area for search-widget
  */
-function yoast_add_top_right_area() {
+function yst_add_top_right_area() {
 	if ( true ) {
 		genesis_widget_area( 'yoast-top-right', array(
 			'before' => '<div id="yoast-top-right" class="widget-area yoast-top-right-widget">',
@@ -143,13 +179,14 @@ function yoast_add_top_right_area() {
 	}
 }
 
-add_action( 'genesis_header', 'yoast_add_top_right_area' );
-
-// Add Read More Link to Excerpts
-add_filter( 'excerpt_more', 'get_read_more_link' );
-add_filter( 'the_content_more_link', 'get_read_more_link' );
-function get_read_more_link() {
-	return '...&nbsp;<div class="exerptreadmore"><a href="' . get_permalink() . '">Read more</a></div>';
+/**
+ *
+ * @todo make sure this has a filter.
+ *
+ * @return string
+ */
+function yst_get_read_more_link() {
+	return '...&nbsp;<div class="exerptreadmore"><a href="' . get_permalink() . '">' . __( 'Read more', 'yoast-theme' ) . '</a></div>';
 }
 
 /**
@@ -180,31 +217,64 @@ function get_read_more_link() {
 function yst_header_sidr_css() {
 	?>
 	<style>
-		@media (min-width: 640px) {
-			a.open, #sidr-menu {
+		@media (min-width: 639px) {
+			a.open, #sidr-left, #sidr-right {
 				width: 0 !important;
 				display: none !important;
 			}
 		}
+
 		@media (max-width: 639px) {
-			a.open {
-				display: block;
-				text-indent: -10000px;
-				width: 35px;
-				height: 24px;
-				line-height: 22px;
-				padding: 15px;
-				margin: 0;
-				background: #333 url('<?php echo get_stylesheet_directory_uri(); ?>/images/hamburger.png') 5px 5px no-repeat;
-			}
-			#yst-nav {
+			.nav-primary {
 				display: none;
+			}
+
+			a.open {
+				display: inline-block;
+				text-indent: -10000px;
+				z-index: 13;
+				width: 25px;
+				height: 25px;
+				line-height: 22px;
+				padding: 10px;
+				margin: 0;
+			}
+
+			a#sidr-left {
+				float: left;
+				background: url('<?php echo get_stylesheet_directory_uri(); ?>/images/hamburger.png') 10px 10px no-repeat;
+			}
+
+			a#sidr-right {
+				float: right;
+				background: url('<?php echo get_stylesheet_directory_uri(); ?>/images/hamburger.png') 10px 10px no-repeat;
+			}
+
+			.nav-primary {
+				display: none !important;
+			}
+
+			body.sticky-menu .site-container a#sidr-left {
+				position: fixed;
+				top: 0;
+				left: 5px;
+			}
+
+			body.sticky-menu .site-container a#sidr-right {
+				position: fixed;
+				top: 0;
+				right: 5px;
+			}
+
+			body.sticky-menu .site-header .title-area .site-title a {
+				position: fixed;
+				top: 0;
+				margin-left: 50px;
 			}
 		}
 	</style>
-	<?php
+<?php
 }
-add_action( 'wp_head', 'yst_header_sidr_css' );
 
 /**
  * Includes SIDR and its stylesheet
@@ -217,8 +287,6 @@ function yst_include_sidr() {
 	wp_enqueue_style( 'yst-sidr-css', get_stylesheet_directory_uri() . '/lib/css/jquery.sidr.dark.css' );
 }
 
-add_action( 'wp_enqueue_scripts', 'yst_include_sidr' );
-
 /**
  * Activates the sidr functionality, allowing for a left hand menu block.
  *
@@ -226,83 +294,58 @@ add_action( 'wp_enqueue_scripts', 'yst_include_sidr' );
  *
  * @link http://www.berriart.com/sidr/#documentation
  *
- * @todo combine with sticky menu code.
+ * @todo check whether yPos in sticky menu code is the right position to switch.
  */
-function yst_activate_sidr() {
-?>
+function yst_activate_sidr_and_sticky_menu() {
+	?>
 	<script>
-		jQuery(document).ready(function($) {
-			$('#sidr-open').sidr({
-				name: 'sidr-menu',
-				source: '.search-form,#yst-nav',
+		jQuery(document).ready(function ($) {
+			$('#sidr-left').sidr({
+				name       : 'sidr-menu-left',
+				source     : function () {
+					return "<h1><?php _e("Navigation","yoast-theme"); ?></h1><ul>" + $('.menu-primary').html() + "</ul>";
+				},
 				coverScreen: true
+			});
+			$('#sidr-right').sidr({
+				name       : 'sidr-menu-right',
+				source     : function () {
+					return "<h1><?php echo sprintf( __("Search %s","yoast-theme"), get_bloginfo('name') ); ?></h1>" + '<?php echo get_search_form(); ?>';
+				},
+				coverScreen: true,
+				side       : 'right'
+			});
+			$(window).scroll(function() {
+				var yPos = ( $(window).scrollTop() );
+				if(yPos > 70) {
+					$("body").addClass("sticky-menu");
+				} else {
+					$("body").removeClass("sticky-menu");
+				}
 			});
 		});
 	</script>
 <?php
 }
-add_action( 'wp_footer', 'yst_activate_sidr' );
 
 /**
- * This adds an ID to the nav element.
- *
- * @param $output
- *
- * @return mixed
- *
- * @todo fix this, should be possible to do this using a genesis attribute filter instead of a string replace.
- *
+ * Output mobile navigation links
  */
-function yst_override_nav_menu( $output ) {
-	return str_replace( '<nav ', '<a class="open" id="sidr-open" href="#sidr-menu">&nbsp;Open navigation</a><nav id="yst-nav" ', $output );
+function yst_mobile_nav() {
+	echo '<a class="open" id="sidr-left" href="#sidr-left">' . __( 'Open Navigation', 'yoast-theme' ) . '</a>';
+	echo '<a class="open" id="sidr-right" href="#sidr-right">' . __( 'Open Search', 'yoast-theme' ) . '</a>';
 }
-add_filter( 'genesis_markup_nav-primary_output', 'yst_override_nav_menu' );
 
 /**
- * Function Do Genesis Footer
+ * Replace the Genesis footer creds text with our own template.
+ *
+ * @param string $footer_creds_text
+ *
+ * @return string
  */
-function yst_do_genesis_footer() {
-	//* Build the text strings.
-	$backtotop_text = '';
-	$creds_text     = sprintf( '&#x000B7; Copyright &copy; %s &#x000B7; %s uses %s by %s and is powered by <a href="http://www.wordpress.org">WordPress</a> &#x000B7;', date( 'Y' ), '<a href="' . home_url() . '">' . get_bloginfo( 'name' ) . '</a>', '<a href="http://yoast.com" rel="nofollow">Theme001</a>', 'Yoast' );
-
-	//* Filter the text strings
-	$backtotop_text = apply_filters( 'genesis_footer_backtotop_text', $backtotop_text );
-	$creds_text     = apply_filters( 'genesis_footer_creds_text', $creds_text );
-
-	$backtotop = $backtotop_text ? sprintf( '<div class="gototop">%s</div>', $backtotop_text ) : '';
-	$creds     = $creds_text ? sprintf( '<div class="creds">%s</div>', $creds_text ) : '';
-
-	$output = $backtotop . $creds;
-
-	echo apply_filters( 'genesis_footer_output', $output, $backtotop_text, $creds_text );
+function yst_footer_creds_text( $footer_creds_text ) {
+	return sprintf( __( '&#x000B7; Copyright &copy; %s &#x000B7; %s uses %s by %s and is powered by <a href="http://www.wordpress.org">WordPress</a> &#x000B7;', 'yoast-theme' ), date( 'Y' ), '<a href="' . home_url() . '">' . get_bloginfo( 'name' ) . '</a>', '<a href="http://yoast.com/wordpress/themes/tailor-made/" rel="nofollow">Tailor Made</a>', 'Yoast' );
 }
-
-remove_action( 'genesis_footer', 'genesis_do_footer' );
-add_action( 'genesis_footer', 'yst_do_genesis_footer' );
-
-/**
- * Add sticky menu for scrolling
- */
-function yst_sticky_menu() {
-	echo '<script>jQuery(function( $ ){
-		if ( $(window).width() > 768 ) {
-			$(window).scroll(function() {
-				var yPos = ( $(window).scrollTop() );
-				if(yPos > 70) {
-					$("body").addClass("sticky");
-				} else {
-					$("body").removeClass("sticky");
-				}
-			});
-		}
-	});</script>';
-}
-add_action( 'wp_footer', 'yst_sticky_menu' );
-
-//* Reposition the breadcrumbs
-remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
-add_action( 'genesis_after_header', 'genesis_do_breadcrumbs' );
 
 /**
  * Displays a term archive intro
@@ -317,13 +360,14 @@ function yoast_term_archive_intro() {
 	echo '<div class="term-intro">';
 	echo '<h1>' . single_term_title( '', false ) . '</h1>';
 	echo '<div class="entry-content">';
+	/**
+	 * This action allows you to output extra content in a term archive intro section.
+	 */
 	do_action( 'yst_term_archive_intro' );
 	echo wpautop( term_description() );
 	echo '</div>';
 	echo '</div>';
 }
-
-add_action( 'genesis_before_loop', 'yoast_term_archive_intro', 20 );
 
 /**
  * Changes the gravatar size to 100
@@ -331,10 +375,10 @@ add_action( 'genesis_before_loop', 'yoast_term_archive_intro', 20 );
  * @param array $args
  *
  * @return array
+ *
+ * @todo make sure this is filterable.
  */
-function sp_comments_gravatar( $args ) {
-    $args['avatar_size'] = 100;
-    return $args;
+function yst_comments_gravatar( $args ) {
+	$args['avatar_size'] = 100;
+	return $args;
 }
-
-add_filter( 'genesis_comment_list_args', 'sp_comments_gravatar' );
