baf2e0afa2df9c7bbd849aef8490e9090aca1869
jdevalk <joost@yoast.com>
2013-12-17 10:59:44 +0000

    Change comment callback and comment output, fixes #21

--- functions.php
+++ functions.php
@@ -588,3 +588,83 @@ if ( function_exists( 'add_image_size' ) ) {
 	add_image_size( 'sidebarfeatured-thumb', 230, 153, true );
 	add_image_size( 'fullwidth-thumb', 290, 193, true );
 }
+
+/**
+ * Comment List Arguments, modify to change the callback function
+ *
+ * @param array $args
+ *
+ * @return array
+ */
+function yst_comment_list_args( $args ) {
+	$args['callback'] = 'yst_comment_callback';
+	return $args;
+}
+
+add_filter( 'genesis_comment_list_args', 'yst_comment_list_args' );
+
+/**
+ * Comment Callback Function
+ *
+ * @param stdClass $comment
+ * @param array    $args
+ * @param integer  $depth
+ */
+function yst_comment_callback( $comment, $args, $depth ) {
+	$GLOBALS['comment'] = $comment; ?>
+
+<li <?php comment_class(); ?> id="comment-<?php comment_ID(); ?>">
+	<article <?php echo genesis_attr( 'comment' ); ?>>
+
+		<?php do_action( 'genesis_before_comment' ); ?>
+
+		<header class="comment-header">
+			<p <?php echo genesis_attr( 'comment-author' ); ?>>
+				<?php
+
+				$author = get_comment_author();
+				$url    = get_comment_author_url();
+
+				if ( ! empty( $url ) && 'http://' !== $url ) {
+					$author = sprintf( '<a href="%s" rel="external nofollow" itemprop="url">%s</a>', esc_url( $url ), $author );
+				}
+
+				printf( 'By <span itemprop="name">%s</span> ', $author );
+
+				$pattern = '<time itemprop="commentTime" datetime="%s"><a href="%s" itemprop="url">%s %s %s</a></time>';
+				printf( $pattern, esc_attr( get_comment_time( 'c' ) ), esc_url( get_comment_link( $comment->comment_ID ) ), esc_html( get_comment_date() ), __( 'at', 'yoast-theme' ), esc_html( get_comment_time() ) );
+				?>
+			</p>
+		</header>
+		<div class="avatar">
+			<?php
+			$avatar_size = 1 == $depth ? 126 : 80;
+			echo get_avatar( $comment, $avatar_size );
+			?>
+		</div>
+		<div class="comment-content" itemprop="commentText">
+			<?php if ( ! $comment->comment_approved ) : ?>
+				<p class="alert"><?php echo apply_filters( 'genesis_comment_awaiting_moderation', __( 'Your comment is awaiting moderation.', 'genesis' ) ); ?></p>
+			<?php endif; ?>
+
+			<?php comment_text(); ?>
+
+			<p class="comment-actions">
+			<?php
+			comment_reply_link( array_merge( $args, array(
+				'depth'  => $depth,
+				'before' => '<span class="comment-reply">',
+				'after'  => '</span>',
+			) ) );
+			edit_comment_link( __( 'Edit comment', 'yoast-theme' ), ' <span class="edit">', '</span>' );
+			?>
+			</p>
+		</div>
+
+		<?php do_action( 'genesis_after_comment' ); ?>
+
+	</article>
+	<?php
+	//* No ending </li> tag because of comment threading
+
+}
