a9b0337ab8395efe0d26da67af1168f49602289f
michielheijmans <michiel@yoast.com>
2014-01-21 19:47:49 +0100

    adjustments for responsive everything some header alignments background image nav links changed to border-bottom added background color for responsive header added color variations
    fixes #18
    relates to #4

--- functions.php
+++ functions.php
@@ -565,13 +565,13 @@ function yst_display_logo() {
 		}
 
 		if ( ! $use_alt_positioning ) {
-			$css .= '@media(max-width: 640px){header.site-header {background:#fff url(' . $mobile_logo . ') no-repeat 50% 0;}}';
+			$css .= '@media(max-width: 640px){header.site-header {background:#fff url(' . $mobile_logo . ') no-repeat 50% 2px;}}';
 		} else {
 			$mobile_logo_height = $yst_mobile_logo_details['height'] - 41;
 			if ( is_user_logged_in() ) {
 				$mobile_logo_height -= 46;
 			}
-			$css .= '@media(max-width:640px){.site-container{padding-top:' . $mobile_logo_height . 'px;background:#fff url(' . $mobile_logo . ') no-repeat 50% 0;background-size: auto;}}';
+			$css .= '@media(max-width:640px){.site-container{padding-top:' . $mobile_logo_height . 'px;background:#fff url(' . $mobile_logo . ') no-repeat 50% 2px;background-size: auto;}}';
 		}
 	}
 
