fcf2f2dec174338c835e0a328ae417b612771236
Barry Kooij <git@barrykooij.nl>
2014-02-07 12:01:55 +0100

    License message update

--- class-theme.php
+++ class-theme.php
@@ -243,7 +243,7 @@ abstract class Yoast_Theme implements iYoast_Theme {
 	public function display_license_admin_notice() {
 
 		echo '<div class="error"><p>';
-		printf( __( 'The Yoast license key is not valid', 'yoast-theme' ), '%URL%' );
+		printf( __( '<b>Warning!</b> The %s theme license key is not valid, you\'re missing out on updates and support! <a href="%s">Enter your license key</a> or <a href="%s">get a license here</a>.', 'yoast-theme' ), $this->get_name(), '%URL%', 'https://yoast.com/wordpress/themes/' . sanitize_title_with_dashes( $this->get_name() ) );
 		echo "</p></div>";
 	}
 
