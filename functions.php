993d1f87f8629ca278ffeda3f67a88c67985fc23
Barry Kooij <git@barrykooij.nl>
2014-02-07 17:31:28 +0100

    Trying to fix the customizer

--- functions.php
+++ functions.php
@@ -1,7 +1,13 @@
 <?php
 
+require_once( get_stylesheet_directory() . '/lib/class-versatile.php' );
+new Yoast_Versatile();
+
+/*
 function __yoast_main() {
 	require_once( get_stylesheet_directory() . '/lib/class-versatile.php' );
 	new Yoast_Versatile();
 }
-add_action( 'genesis_setup', '__yoast_main', 15 );
+add_action( 'after_setup_theme', '__yoast_main', 1 );
+*/
+//add_action( 'genesis_setup', '__yoast_main', 15 );
