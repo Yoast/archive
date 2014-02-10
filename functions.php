bd0b4374b856db17a2dcd6b6da79d97302dc1429
Barry Kooij <git@barrykooij.nl>
2014-02-10 20:26:36 +0100

    Removed unused code

--- functions.php
+++ functions.php
@@ -2,12 +2,3 @@
 
 require_once( get_stylesheet_directory() . '/lib/class-versatile.php' );
 new Yoast_Versatile();
-
-/*
-function __yoast_main() {
-	require_once( get_stylesheet_directory() . '/lib/class-versatile.php' );
-	new Yoast_Versatile();
-}
-add_action( 'after_setup_theme', '__yoast_main', 1 );
-*/
-//add_action( 'genesis_setup', '__yoast_main', 15 );
