874ee66ba22a6b729cff778ba8a17b92d0ec5a60
Barry Kooij <git@barrykooij.nl>
2014-02-10 20:30:34 +0100

    Removed unused code

--- functions.php
+++ functions.php
@@ -1,9 +1,4 @@
 <?php
 
-function __yoast_main() {
-	require_once( get_stylesheet_directory() . '/lib/class-vintage.php' );
-	new Yoast_Vintage();
-}
-add_action( 'genesis_setup', '__yoast_main', 15 );
-
-require_once 'old-functions.php';
+require_once( get_stylesheet_directory() . '/lib/class-vintage.php' );
+new Yoast_Vintage();
