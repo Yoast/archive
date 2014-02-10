f20015737a978c72e194fa58623bb52799d35b69
Barry Kooij <git@barrykooij.nl>
2014-02-10 20:30:15 +0100

    Removed unused code

--- functions.php
+++ functions.php
@@ -1,8 +1,4 @@
 <?php
 
-function __yoast_main() {
-	require_once( get_stylesheet_directory() . '/lib/class-tailor-made.php' );
-	new Yoast_Tailor_Made();
-}
-
-add_action( 'genesis_setup', '__yoast_main', 15 );
+require_once( get_stylesheet_directory() . '/lib/class-tailor-made.php' );
+new Yoast_Tailor_Made();
