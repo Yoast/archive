06fd5d475a7e5008211a3e603ca1c2c9ccba0468
Barry Kooij <git@barrykooij.nl>
2014-03-07 14:21:35 +0100

    Update manager is now using the Yoast_Product

--- class-update-manager.php
+++ class-update-manager.php
@@ -1,82 +1,51 @@
 <?php
 
 class Yoast_Update_Manager {
-	
-	/**
-	* @var string
-	*/
-	protected $api_url;
 
 	/**
-	* @var string
-	*/
-	protected $item_name;
+	 * @var Yoast_Product
+	 */
+	protected $product;
 
 	/**
-	* @var string
-	*/
-	protected $slug;
-
-	/**
-	* @var string
-	*/
+	 * @var string
+	 */
 	protected $license_key;
 
 	/**
-	* @var string
-	*/
-	protected $version;
-
-	/**
-	* @var string
-	*/
-	protected $author;
-
-	/**
-	* @var string
-	*/
-	protected $text_domain = 'yoast';
-
-	/**
-	* @var WP_Error 
-	*/
+	 * @var WP_Error
+	 */
 	protected $wp_error;
 
 	/**
-	* Constructor
-	*
-	* @param string $api_url The url to the EDD shop
-	* @param string $item_name The item name in the EDD shop
-	* @param string $license_key The (valid) license key
-	* @param string $slug The slug. This is either the plugin main file path or the theme slug.
-	* @param string $version The current plugin or theme version
-	* @param string $author (optional) The item author.
-	*/
-	public function __construct( $api_url, $item_name, $license_key, $slug, $version, $author, $text_domain ) {
-
-		$this->api_url = $api_url;
-		$this->item_name = $item_name;
+	 * Constructor
+	 *
+	 * @param string $api_url     The url to the EDD shop
+	 * @param string $item_name   The item name in the EDD shop
+	 * @param string $license_key The (valid) license key
+	 * @param string $slug        The slug. This is either the plugin main file path or the theme slug.
+	 * @param string $version     The current plugin or theme version
+	 * @param string $author      (optional) The item author.
+	 */
+	public function __construct( Yoast_Product $product, $license_key ) {
+		$this->product     = $product;
 		$this->license_key = $license_key;
-		$this->slug = $slug;
-		$this->version = $version;
-		$this->author = $author;
-		$this->text_domain = $text_domain;
 	}
 
 	/**
-	* If the update check returned a WP_Error, show it to the user
-	*/ 
+	 * If the update check returned a WP_Error, show it to the user
+	 */
 	public function show_update_error() {
-		
-		if( $this->wp_error === null ) {
+
+		if ( $this->wp_error === null ) {
 			return;
 		}
 
 		?>
 		<div class="error">
-			<p><?php printf( __( '%s failed to check for updates because of the following error: <em>%s</em>', $this->text_domain ), $this->item_name, $this->wp_error->get_error_message() ); ?></p>
+			<p><?php printf( __( '%s failed to check for updates because of the following error: <em>%s</em>', $this->product->get_text_domain() ), $this->product->get_item_name(), $this->wp_error->get_error_message() ); ?></p>
 		</div>
-		<?php
+	<?php
 	}
 
 	/**
@@ -94,36 +63,37 @@ class Yoast_Update_Manager {
 		$api_params = array(
 				'edd_action' => 'get_version',
 				'license'    => $this->license_key,
-				'name'       => $this->item_name,
-				'slug'       => $this->slug,
-				'author'     => $this->author
+				'name'       => $this->product->get_item_name(),
+				'slug'       => $this->product->get_slug(),
+				'author'     => $this->product->get_author()
 		);
 
 		// setup request parameters
-		$request_params = array( 
-			'timeout' => 15, 
-			'sslverify' => false, 
-			'body' => $api_params 
+		$request_params = array(
+				'timeout'   => 15,
+				'sslverify' => false,
+				'body'      => $api_params
 		);
 
 		// call remote api
-		$response = wp_remote_post( $this->api_url, $request_params );
+		$response = wp_remote_post( $this->product->get_api_url(), $request_params );
 
 
 		// wp / http error?
-		if( is_wp_error( $response) ) {
+		if ( is_wp_error( $response ) ) {
 			$this->wp_error = $response;
 
 			// show error to user
-			add_action( 'admin_notices', array( $this, 'show_update_error') );
+			add_action( 'admin_notices', array( $this, 'show_update_error' ) );
 
 			return false;
 		}
 
 
 		// decode response
-		$response = json_decode( wp_remote_retrieve_body( $response ) );
+		$response           = json_decode( wp_remote_retrieve_body( $response ) );
 		$response->sections = maybe_unserialize( $response->sections );
+
 		return $response;
 	}
 
