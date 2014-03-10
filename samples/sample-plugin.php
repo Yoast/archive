07f5799224dc37971d16952324fd5c170a659a84
Barry Kooij <git@barrykooij.nl>
2014-03-10 11:39:32 +0100

    Rewrote sample plugin

--- samples/sample-plugin.php
+++ samples/sample-plugin.php
@@ -13,54 +13,37 @@ Text Domain: sample-plugin
 /**
  * Class Sample_Plugin
  *
- * @todo example needs to be rewritten
  */
 class Sample_Plugin {
 
-	private $api_url = 'http://localhost/wp/latest/';
-	private $item_name = 'Sample Plugin';
-	private $item_url = '';
-	private $version = '1.0';
-	private $text_domain = 'sample-plugin';
-	private $file_slug;
-	private $author = 'Yoast';
-
-
-	/**
-	* @var Yoast_Plugin_License_Manager the license manager instance
-	*/ 
-	private $license_manager;
-
 	public function __construct() {
 
-		$this->file_slug = plugin_basename( __FILE__ );
-
 		// we only need license stuff inside the admin area
-		if( is_admin() ) {
+		if ( is_admin() ) {
 
 			// add menu item
-			add_action( 'admin_menu', array( $this, 'add_license_menu') ); 
+			add_action( 'admin_menu', array( $this, 'add_license_menu' ) );
 
 			// load license class
-			$this->load_license_manager();			
+			$this->load_license_manager();
 		}
 
-		
+
 	}
 
 	/**
-	* Loads the License_Plugin_Manager class
-	*
-	* The class will take care of the rest: notices, license (de)activations, updates, etc..
-	*/
+	 * Loads the License_Plugin_Manager class
+	 *
+	 * The class will take care of the rest: notices, license (de)activations, updates, etc..
+	 */
 	public function load_license_manager() {
-		// load license manager
-			$options_page = 'options.php?page=' . $this->text_domain . '-license';
-			require_once dirname( __FILE__ ) . '/License-Manager/class-license-manager.php';
-			require_once dirname( __FILE__ ) . '/License-Manager/class-plugin-license-manager.php';
 
-			// instantiate license class, this is all we need
-			$this->license_manager = new Yoast_Plugin_License_Manager( $this->api_url, $this->item_name, $this->file_slug, $this->version, $this->item_url, $options_page, $this->text_domain, $this->author );
+		// Instantiate license class
+		$license_manager = new Yoast_Plugin_License_Manager( new Sample_Product() );
+
+		// Setup the required hooks
+		$license_manager->setup_hooks();
+
 	}
 
 	/**
@@ -69,18 +52,22 @@ class Sample_Plugin {
 	public function add_license_menu() {
 		$theme_page = add_options_page( sprintf( __( '%s License', $this->text_domain ), $this->item_name ), sprintf( __( '%s License', $this->text_domain ), $this->item_name ), 'manage_options', $this->text_domain . '-license', array( $this, 'show_license_page' ) );
 	}
-	
+
 	/**
-	* Shows license page
-	*/
+	 * Shows license page
+	 */
 	public function show_license_page() {
+
+		// Instantiate license class
+		$license_manager = new Yoast_Plugin_License_Manager( new Sample_Product() );
+
 		?>
 		<div class="wrap">
 			<?php //settings_errors(); ?>
 
-			<?php $this->license_manager->show_license_form( false ); ?>
+			<?php $license_manager->show_license_form( false ); ?>
 		</div>
-		<?php
+	<?php
 	}
 }
 
