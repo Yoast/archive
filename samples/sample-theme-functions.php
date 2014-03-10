0510eedfe7ad39cfa602c7814da63c000a9ec836
Barry Kooij <git@barrykooij.nl>
2014-03-10 11:39:10 +0100

    Rewrote sample theme

--- samples/sample-theme-functions.php
+++ samples/sample-theme-functions.php
@@ -1,25 +1,11 @@
 <?php
 
-/**
- * @todo example needs to be rewritten
- */
-
 if( is_admin() ) {
 
-	// setup required variables
-	$api_url 		= 'https://yoast.com';
-	$item_name 		= 'Sample Theme';
-	$item_url		= 'http://dannyvankooten.com/themes/sample-theme/';
-	$version 		= '1.1';
-	$text_domain 	= 'sample-theme';
-	$theme_slug		= 'sample-theme';
-	$author 		= 'Yoast';
+	// Instantiate license class
+	$license_manager = new Yoast_Theme_License_Manager( new Sample_Product() );
 
-	// load classes
-	require_once dirname( __FILE__ ) . '/License-Manager/class-license-manager.php';
-	require_once dirname( __FILE__ ) . '/License-Manager/class-theme-license-manager.php';
-
-	// instantiate license class
-	$license_manager = new Yoast_Theme_License_Manager( $api_url, $item_name, $theme_slug, $version, $item_url, $text_domain, $author );
+	// Setup the required hooks
+	$license_manager->setup_hooks();
 
 }
