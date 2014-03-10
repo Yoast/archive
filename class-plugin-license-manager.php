b37ad0218a4849da4de70bc71de809891ec3b70e
Barry Kooij <git@barrykooij.nl>
2014-03-10 11:39:25 +0100

    Removed todo & code style

--- class-plugin-license-manager.php
+++ class-plugin-license-manager.php
@@ -1,27 +1,26 @@
 <?php
-if( ! class_exists( "Yoast_Plugin_License_Manager" ) ) {
+if ( ! class_exists( "Yoast_Plugin_License_Manager" ) ) {
 
 	class Yoast_Plugin_License_Manager extends Yoast_License_Manager {
 
 		/**
-		* Setup auto updater for plugins
-		*/
+		 * Setup auto updater for plugins
+		 */
 		public function setup_auto_updater() {
-			if( $this->license_is_valid() ) {
+			if ( $this->license_is_valid() ) {
 				// setup auto updater
-			// @todo is require really needed here? require_once might be better.
 				require_once dirname( __FILE__ ) . '/class-plugin-update-manager.php';
-			new Yoast_Plugin_Update_Manager( $this->product, $this->get_license_key() );
+				new Yoast_Plugin_Update_Manager( $this->product, $this->get_license_key() );
 			}
 		}
-		
+
 		/**
-		* Setup hooks
-		*/
+		 * Setup hooks
+		 */
 		public function specific_hooks() {
 
 			// deactivate the license remotely on plugin deactivation
-		register_deactivation_hook( $this->product->get_slug(), array($this, 'deactivate_license') );
+			register_deactivation_hook( $this->product->get_slug(), array( $this, 'deactivate_license' ) );
 		}
 	}
 }
