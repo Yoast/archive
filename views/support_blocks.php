4a366794748ad6016510aca63d301fbe23278399
Peter van Wilderen <peter@yoast.com>
2014-10-02 09:57:53 +0200

    Support block url fix in translation

--- views/support_blocks.php
+++ views/support_blocks.php
@@ -1,3 +1,3 @@
 <h2 id="wpseo-title"><?php echo __('Yoast Support', 'yoast-support-framework'); ?></h2>
 
-<p><?php echo __('You need help? We are there for you. Before you ask us, check our <a href="https://yoast.com/support/" target="_blank">Support page</a>. Please fill in this field and we will answer your question soon.', 'yoast-support-framework'); ?></p>
+<p><?php printf( __('You need help? We are there for you. Before you ask us, check our %s. Please fill in this field and we will answer your question soon.', 'yoast-support-framework'), '<a href="https://yoast.com/support/" target="_blank">Support page</a>' ); ?></p>
