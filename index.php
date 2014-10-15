b03235ff4c2210455cb80f6a27c10db7c6116a1e
Taco Verdonschot <taco@yoast.com>
2014-10-15 12:06:35 +0200

    Sharing some first tests

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+	<meta charset="UTF-8">
+	<title>Image Uploadizer</title>
+	<script type="text/javascript" src="uploadizer.js"></script>
+</head>
+<body>
+<p>This is the Image Uploadizer.</p>
+
+<div id="form">
+	<form>
+		<input type="file" name="upload_file" accept="image/*" />
+	</form>
+</div>
+<div id="imagepreview"></div>
+</body>
+</html>
