0a602134a570fbc3f2bd5296a6366647f0e6afd9
Jip Moors <jip@yoast.com>
2016-03-11 09:26:49 +0100

    Sanitize analytics code.

--- classes/class-options.php
+++ classes/class-options.php
@@ -92,6 +92,8 @@ if ( ! class_exists( 'YoastSEO_AMP_Options' ) ) {
 				}
 			}
 
+			$options['analytics-extra'] = $this->sanitize_analytics_code( $options['analytics-extra'] );
+
 			return $options;
 		}
 
@@ -110,6 +112,49 @@ if ( ! class_exists( 'YoastSEO_AMP_Options' ) ) {
 			}
 
 			return $default;
+		}
+
+		/**
+		 * @param string $source Raw input.
+		 *
+		 * @return string Sanitized code.
+		 */
+		private function sanitize_analytics_code( $source ) {
+
+			$source = trim( $source );
+
+			if ( empty( $source ) ) {
+				return $source;
+			}
+
+			$code = $source;
+
+			// Strip all tags, to verify JSON input.
+			$json        = strip_tags( $code );
+			$parsed_json = json_decode( $json, true );
+
+			// Non-parsable JSON is always bad.
+			if ( is_null( $parsed_json ) ) {
+				return '';
+			}
+
+			$allowed_tags = strip_tags( $code, '<amp-analytics>' );
+
+			// Strip JSON content so we can apply verified script tag.
+			$tag = str_replace( $json, '', $allowed_tags );
+
+			$parts = explode( '><', $tag );
+			$parts[0] .= '>';
+			$parts[1] = '<' . $parts[1];
+
+			// Rebuild with script tag and json content.
+			array_splice( $parts, 1, null, array(
+				'<script type="application/json">',
+				trim( $json ),
+				'</script>'
+			) );
+
+			return implode( "\n", $parts );
 		}
 
 		/**
