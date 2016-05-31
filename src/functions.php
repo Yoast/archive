bca7082d36be214cad06823d2c64612592ea8a32
diedexx <diedex10@yahoo.com>
2016-05-31 13:11:21 +0200

    can change beacon type from yoast_get_helpscout_beacon function now

--- src/functions.php
+++ src/functions.php
@@ -11,11 +11,11 @@ if ( ! function_exists( 'yoast_get_helpscout_beacon' ) ) {
 	 *
 	 * @return Yoast_HelpScout_Beacon
 	 */
-	function yoast_get_helpscout_beacon( $page ) {
+	function yoast_get_helpscout_beacon( $page, $type = Yoast_HelpScout_Beacon::BEACON_TYPE_SEARCH ) {
 		static $beacon;
 
 		if ( ! isset( $beacon ) ) {
-			$beacon = new Yoast_HelpScout_Beacon( $page, array() );
+			$beacon = new Yoast_HelpScout_Beacon( $page, array(), $type );
 		}
 
 		return $beacon;
