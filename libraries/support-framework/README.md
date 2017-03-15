dfcfe0239614359cfb7ad18049d67c6874bd5366
omarreiss <omar.reiss@gmail.com>
2017-03-15 14:25:11 +0100

    remove support framework

--- libraries/support-framework/README.md
+++
@@ -1,4 +0,0 @@
-Support-Framework
-=================
-
-Support Framework
--- libraries/support-framework/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo $yoast_support->get_view('support_blocks');
-echo $yoast_support->get_view('admin_details');
-echo $yoast_support->get_view('form');
-?>
