3b2772b9685e13696f020bfa87e8e438523eb88d
Anton Timmermans <email@atimmer.com>
2017-07-06 15:34:43 +0200

    Add two handy WordPress helpers

---
+++ src/WP_CLI_Logger.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Yoast\WordPress;
+
+use Psr\Log\AbstractLogger;
+use Psr\Log\LogLevel;
+
+/**
+ * PSR-3 logger for WP CLI.
+ */
+final class WP_CLI_Logger extends AbstractLogger {
+
+	/**
+	 * Logs with an arbitrary level.
+	 *
+	 * @param mixed  $level
+	 * @param string $message
+	 * @param array  $context
+	 *
+	 * @return void
+	 */
+	public function log( $level, $message, array $context = array() ) {
+		switch ( $level ) {
+			case LogLevel::WARNING:
+				\WP_CLI::warning( $message );
+				break;
+			case LogLevel::ERROR:
+			case LogLevel::ALERT:
+			case LogLevel::EMERGENCY:
+			case LogLevel::CRITICAL:
+				\WP_CLI::error( $message );
+				break;
+			default:
+				\WP_CLI::log( $message );
+		}
+	}
+	}
+}
