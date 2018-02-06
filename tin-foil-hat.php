7d811ef6aa716c82cefce44efc06f621dbd4fb0e
IreneStr <irene@yoast.com>
2018-02-06 09:28:00 +0100

    Add license, package.json and php file

---
+++ tin-foil-hat.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Tin Foil Hat by Yoast
+Description: This plugin helps you write awesome conspiring content.
+Version: 0.1
+Author: Team Yoast
+Author URI: https://yoast.com
+License: GPL v3
+*/
+
