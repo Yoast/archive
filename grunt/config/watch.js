fca85c3e858da0fdb7a7831968791f8d23edf65b
IreneStr <irene@yoast.com>
2018-02-13 09:27:34 +0100

    Add gruntfile and grunt watch task

---
+++ grunt/config/watch.js
@@ -0,0 +1,36 @@
+module.exports = {
+	options: {
+		livereload: true,
+	},
+	grunt: {
+		options: {
+			reload: true,
+		},
+		files: [
+			"<%= files.grunt %>",
+			"<%= files.config %>",
+		],
+		tasks: [
+			"eslint:grunt",
+		],
+	},
+	php: {
+		files: [
+			"<%= files.php %>",
+		],
+		tasks: [
+			"phplint",
+			"phpcs",
+		],
+	},
+	js: {
+		files: [
+			"<%= files.js %>",
+			"<%= paths.js %>/**/*.js",
+		],
+		tasks: [
+			"build:js",
+			"eslint:plugin",
+		],
+	},
+};
