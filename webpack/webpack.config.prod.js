4f1f2a0387f33f29a74a44ec061ac8b861464389
IreneStr <irene@yoast.com>
2018-02-26 11:14:50 +0100

    Fix webpack

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,5 +1,5 @@
 /* global require, module */
-const defaults = require( "./webpack.config.js" ).defaults;
+const defaults = require( "./webpack.config.default.js" ).defaults;
 
 const prodConfig = {
 	devtool: "cheap-module-source-map",
