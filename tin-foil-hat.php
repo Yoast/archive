ece1866d4aaf2a5af7e4553d67248e1862c329af
IreneStr <irene@yoast.com>
2018-02-27 16:04:10 +0100

    Load js files in php

--- tin-foil-hat.php
+++ tin-foil-hat.php
@@ -2,9 +2,31 @@
 /*
 Plugin Name: Tin Foil Hat by Yoast
 Description: This plugin helps you write awesome conspiring content.
-Version: 0.1
+Version: 1.4
 Author: Team Yoast
 Author URI: https://yoast.com
 License: GPL v3
 */
 
+define('YOAST_TIN_FOIL_HAT', __FILE__);
+
+class Yoast_TinFoilHat
+{
+	public function add_hooks()
+	{
+		add_action('admin_enqueue_scripts', array($this, 'enqueue_scripts'));
+	}
+
+	/**
+	 * Enqueues the pluginscripts.
+	 */
+	public function enqueue_scripts()
+	{
+		// only enqueue on x-edit page.
+		// todo: dist file
+		wp_enqueue_script('yoast-tfh', plugins_url('js/src/tin-foil-hat-plugin.js', YOAST_TIN_FOIL_HAT ), array(), '1.4', true);
+	}
+}
+
+$yoast_tinfoilhat = new Yoast_TinFoilHat();
+$yoast_tinfoilhat->add_hooks();
