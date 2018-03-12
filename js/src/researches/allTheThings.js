dc8db5863b442bfc0b4887cd312e6d0383d09636
terw-dan <danny@yoast.com>
2018-03-12 18:32:30 +0100

    Adds checks for all the things

--- js/src/researches/allTheThings.js
+++ js/src/researches/allTheThings.js
@@ -1,1 +1,28 @@
-let allTheThings = [ "moon landing", "flat earth", "ufo", "area 51", "denver airport", "illuminati", "freemasons", "reptilians", "chemtrails"  ]
+let allTheThings = [ "moon landing", "earth is flat", "ufo", "area 51", "denver airport", "illuminati", "freemasons", "reptilians", "chemtrails"  ];
+let toRegex = require( "yoastseo/js/stringProcessing/createWordRegex.js" );
+
+
+const matchAllThethings = function( text ){
+	let i, missingMatch = "";
+
+	for ( i = 0; i < allTheThings.length; i++ ) {
+		let match = toRegex( allTheThings[ i ] );
+		if( ! text.match( match ) ) {
+			missingMatch =  allTheThings[ i ];
+			break;
+		}
+	}
+	return missingMatch;
+};
+
+
+/**
+ * Checks a text for a list of things you should write about.
+ *
+ * @param {Object} paper The paper to check for mentions of important things.
+ * @returns {String} The first string in the array that isn't matched. Empty if no more things need to be matched.
+ */
+module.exports = function( paper ) {
+	let text = paper.getText();
+	return matchAllThethings( text );
+};
--- js/src/tests/allTheThings.test.js
+++ js/src/tests/allTheThings.test.js
@@ -1,11 +1,28 @@
-let allThethings = require( "../researches/allTheThings" );
+let allTheThings = require( "../researches/allTheThings" );
 let Paper = require( "yoastseo/js/values/Paper.js" );
 
 describe( "This will check if you've written all the things people need to know!", function(){
 	it ( "returns the moonlanding as the next thing to write about", function(){
-			let mockPaper = new Paper( "This is an ordinary text." );
-			let things = allTheThings( mockPaper );
-			expect( things ).toEqual( "the moon landing" );
-		});
+		let mockPaper = new Paper( "This is an ordinary text." );
+		let thing = allTheThings( mockPaper );
+		expect( thing ).toEqual( "moon landing" );
+	});
+
+	it ( "returns flat earth as the next thing to write about", function(){
+		let mockPaper = new Paper( "The moon landing was staged" );
+		let thing = allTheThings( mockPaper );
+		expect( thing ).toEqual( "earth is flat" );
+	});
+
+	it ( "returns ufo as the next thing to write about", function(){
+		let mockPaper = new Paper( "The moon landing was staged, the earth is flat and run by the reptilians " );
+		let thing = allTheThings( mockPaper );
+		expect( thing ).toEqual( "ufo" );
+	});
+
+	it ("returns an empty string when everything is written.", function(){
+		let mockPaper = new Paper( "moon landing earth is flat ufo area 51 denver airport illuminati freemasons reptilians chemtrails" );
+		let thing = allTheThings( mockPaper );
+		expect( thing ).toEqual( "" );
 	});
 });
