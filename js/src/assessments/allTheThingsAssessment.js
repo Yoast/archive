d5b9e57758334392e0aca1db1d43fd438cae9c6a
terw-dan <danny@yoast.com>
2018-03-19 15:59:40 +0100

    Updates texts

--- js/src/assessments/allTheThingsAssessment.js
+++ js/src/assessments/allTheThingsAssessment.js
@@ -2,12 +2,12 @@
 
 let AssessmentResult = require( "yoastseo/js/values/AssessmentResult" );
 let wordCount = require( "yoastseo/js/stringProcessing/countWords" );
-let getAllTheThings = require( "../researches/allTheThings" );
+let AllTheThings = require( "../researches/allTheThings" );
 
 /**
- * Returns the score object based on the number of conspiracies in a text.
+ * Returns the score object based on the missing thing.
  *
- * @param {Paper} paper The paper to check for conspiracies in.
+ * @param {Paper} paper The paper to check for conspiracies.
  * @returns {{score: number, text:  string}} The object containing the score and feedback text.
  */
 const scoreAllTheThings = function( paper ) {
@@ -15,7 +15,7 @@ const scoreAllTheThings = function( paper ) {
 
 	// Only trigger this assessment when a text contains at least 250 words.
 	if ( wordCount( paper.getText() ) >= 250 ) {
-		let missingThing = getAllTheThings( paper );
+		let missingThing = AllTheThings( paper );
 
 		switch ( missingThing ) {
 			case "moon landing":
@@ -24,19 +24,19 @@ const scoreAllTheThings = function( paper ) {
 				break;
 			case "earth is flat":
 				result.score = 3;
-				result.text = "People really should know how the earth is flat, there is a reason the moon landing was faked!";
+				result.text = "People really should know that the earth is flat. There is a reason the moon landing was faked!";
 				break;
 			case "ufo":
 				result.score = 3;
-				result.text = "While you are on the subject of why the earth is flat. Why not mention how UFO's are involved?!";
+				result.text = "While you are on the subject of why the earth is flat, why not mention how UFOs are involved?!";
 				break;
 			case "area 51":
 				result.score = 3;
-				result.text = "How can you talk about UFO's, without talking about Area 51?!";
+				result.text = "How can you talk about UFOs without talking about Area 51?!";
 				break;
 			case "denver airport":
 				result.score = 3;
-				result.text = "Area 51 isn't the only suspicious place. What about Denver Airport? That place is shady as can be!";
+				result.text = "Area 51 isn't the only suspicious place. What about Denver Airport? That place is as shady as can be!";
 				break;
 			case "illuminati":
 				result.score = 6;
@@ -44,20 +44,20 @@ const scoreAllTheThings = function( paper ) {
 				break;
 			case "freemasons":
 				result.score = 6;
-				result.text = "Do you really think the Illuminati are alone? Of course not, the Freemasons are in on it as well!";
+				result.text = "Do you really think the Illuminati are alone? Of course not! The Freemasons are in on it too!";
 				break;
 			case "reptilians":
 				result.score = 6;
-				result.text = "Our earth is controlled by Reptillians, you know that. Please don't be too hesitant too tell that!!";
+				result.text = "Our earth is controlled by Reptillians! You know that. Please don't be too hesitant too tell that!!!";
 				break;
 			case "chemtrails":
 				result.score = 6;
-				result.text = "Did you see those lines in the sky? Chemtrails! they are spreading doom over the world. " +
+				result.text = "Did you see those lines in the sky? Chemtrails! They are spreading doom over the world. " +
 					"Everyone should know this. Tell them!!";
 				break;
 			default:
 				result.score = 9;
-				result.text = "You know what you are talking about. Great you're willing to share your ultimate knowledge with your readers!!";
+				result.text = "You know what you are talking about!! Great you're willing to share your ultimate knowledge with your readers!!!";
 				break;
 		}
 	}
@@ -65,7 +65,7 @@ const scoreAllTheThings = function( paper ) {
 };
 
 /**
- * Execute the Truthdensity Assessment and return a result.
+ * Execute the All the things Assessment and return a result.
  * @param {Paper} paper The Paper object to assess.
  * @returns {AssessmentResult} The result of the assessment, containing both a score and a feedback text.
  */
