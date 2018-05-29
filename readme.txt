a3d717cd846e7a1c5ef92d4c09bfa45e26573ecb
Jip Moors <jhp.moors@gmail.com>
2018-05-29 09:53:25 +0200

    Add WordPress required structure

---
+++ readme.txt
@@ -0,0 +1,39 @@
+=== Yoast Search Index Purge ===
+Contributors: yoast, joostdevalk, tacoverdo, omarreiss, atimmer, jipmoors
+Donate link: https://yoa.st/1up
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl.html
+Tags: SEO, XML sitemap, Google Search Console, Content analysis, Readability
+Requires at least: 4.8
+Tested up to: 4.9.6
+Stable tag: 1.0.0
+Requires PHP: 5.2.4
+
+Improve your WordPress SEO: Write better content and have a fully optimized WordPress site using the Yoast SEO plugin.
+
+== Description ==
+
+== Installation ==
+
+=== From within WordPress ===
+
+1. Visit 'Plugins > Add New'
+1. Search for 'Yoast SEO: Search Index Purge'
+1. Activate Yoast SEO: Search Index Purge from your Plugins page.
+1. Go to "after activation" below.
+
+=== Manually ===
+
+1. Upload the `yoast-search-index-purge` folder to the `/wp-content/plugins/` directory
+1. Activate the Yoast SEO: Search Index Purge plugin through the 'Plugins' menu in WordPress
+1. Go to "after activation" below.
+
+=== After activation ===
+
+== Frequently Asked Questions ==
+
+You'll find answers to many of your questions on [kb.yoast.com](https://yoa.st/1va).
+
+== Screenshots ==
+
+== Changelog ==
---
+++ search-index-purge.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Yoast SEO: Search index purge
+ *
+ * @package   WPSEO\Main
+ * @copyright Copyright (C) 2018, Yoast BV - support@yoast.com
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License, version 3 or higher
+ *
+ * @wordpress-plugin
+ * Plugin Name: Yoast SEO: Search index purge
+ * Version:     1.0.0
+ * Plugin URI:
+ * Description:
+ * Author:      Team Yoast
+ * Author URI:  https://yoa.st/1uk
+ * Text Domain: yoast-search-index-purge
+ * Domain Path: /languages/
+ * License:     GPL v3
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
