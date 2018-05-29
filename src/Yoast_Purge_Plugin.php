7e4d5d529d0655e80d8a59b7099002e52e4a0485
Jip Moors <jhp.moors@gmail.com>
2018-05-29 11:26:41 +0200

    Introduces the Yoast SEO plugin version requirement check

--- src/Yoast_Purge_Plugin.php
+++ src/Yoast_Purge_Plugin.php
@@ -10,10 +10,14 @@ final class Yoast_Purge_Plugin {
 	 */
 	private $integrations = array();
 
+	/** @var Yoast_Purge_Require_Yoast_SEO_Version */
+	private $requirement_checker;
+
 	/**
 	 * Initializes the plugin.
 	 */
 	public function __construct() {
+		$this->requirement_checker = new Yoast_Purge_Require_Yoast_SEO_Version();
 	}
 
 	/**
@@ -29,12 +33,20 @@ final class Yoast_Purge_Plugin {
 	 * Registers hooks to WordPress.
 	 */
 	public function register_hooks() {
+		// Always show notifications, even if the rest cannot be activated.
+		$this->requirement_checker->register_hooks();
+		if ( ! $this->requirement_checker->has_required_version() ) {
+			return;
+		}
+
 		foreach ( $this->integrations as $integration ) {
 			$integration->register_hooks();
 		}
 	}
 
 	/**
+	 * Retrieves the registered integrations.
+	 *
 	 * @return array
 	 */
 	public function get_integrations() {
