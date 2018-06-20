66ff76b4cb229c48cad08d0f56a0cd5338af93b6
Jip Moors <jhp.moors@gmail.com>
2018-06-20 10:50:32 +0200

    Exclude newly added attachments from the 410-sitemap

--- src/Yoast_Purge_Attachment_Sitemap_Provider.php
+++ src/Yoast_Purge_Attachment_Sitemap_Provider.php
@@ -13,6 +13,64 @@ final class Yoast_Purge_Attachment_Sitemap_Provider extends WPSEO_Post_Type_Site
 	 */
 	public function __construct( Yoast_Purge_Options $options ) {
 		$this->options = $options;
+
+		add_filter( 'wpseo_typecount_where', array( $this, 'typecount_where_filter' ), 10, 2 );
+		add_filter( 'wpseo_exclude_from_sitemap_by_post_ids', array( $this, 'exclude_newly_added_attachments' ), 10, 2 );
+	}
+
+	/**
+	 * Modifies the sitemap count for attachments added after activating the plugin.
+	 *
+	 * @param string $where     SQL part, defaults to empty string.
+	 * @param string $post_type Post type name.
+	 *
+	 * @return string Adjusted WHERE query part.
+	 */
+	public function typecount_where_filter( $where, $post_type ) {
+		global $wpdb;
+
+		// Only hook into attachment sitemaps.
+		if ( $post_type !== 'attachment' ) {
+			return $where;
+		}
+
+		// Only count items added before activating.
+		$where .= $wpdb->prepare(
+			" AND {$wpdb->posts}.post_date <= %s",
+			date( 'Y-m-d H:i:s', $this->options->get_activation_date() )
+		);
+
+		return $where;
+	}
+
+	/**
+	 * Excludes attachments from the sitemap that were added after activating.
+	 *
+	 * @param array $excluded List of excluded post ids.
+	 *
+	 * @return array Modified list of excluded post ids.
+	 */
+	public function exclude_newly_added_attachments( $excluded ) {
+		// Add newly added attachments to the list.
+		$timestamp = $this->options->get_activation_date();
+
+		$wp_query = new WP_Query(
+			array(
+				'post_type' => 'attachment',
+				'post_status' => 'any',
+				'posts_per_page' => '-1',
+				'date_query'     => array(
+					'after' => date( 'Y-m-d H:i:s', $timestamp ),
+				),
+				'fields' => 'ids'
+			)
+		);
+
+		if ( empty( $wp_query->posts ) ) {
+			return $excluded;
+		}
+
+		return array_unique( array_merge( $excluded, $wp_query->posts ) );
 	}
 
 	/**
