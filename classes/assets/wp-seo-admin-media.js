5de6f36a39b58eda41706d24e8d6263c83d96af4
Jip <jhp.moors@gmail.com>
2018-12-18 16:22:10 +0100

    Copy content of wp-seo-admin-media

--- classes/assets/wp-seo-admin-media.js
+++ classes/assets/wp-seo-admin-media.js
@@ -1,36 +1,81 @@
 /* global wpseoMediaL10n */
-/* global ajaxurl */
 /* global wp */
-/* jshint -W097 */
-/* jshint -W003 */
-/* jshint unused:false */
 
 // Taken and adapted from http://www.webmaster-source.com/2013/02/06/using-the-wordpress-3-5-media-uploader-in-your-plugin-or-theme/
-jQuery(document).ready(
-	function($) {
-		'use strict';
-		if( typeof wp.media === 'undefined' ) {
+jQuery( document ).ready(
+	function( $ ) {
+		if ( typeof wp.media === "undefined" ) {
 			return;
 		}
 
-		$('.wpseo_image_upload_button').each(function(index, element) {
-			var wpseo_target_id = $(element).attr('id').replace(/_button$/, '');
-			var wpseo_custom_uploader = wp.media.frames.file_frame = wp.media({
+		/**
+		 * Returns the target HTML id for this button element.
+		 *
+		 * @param {Object} $button The image select button.
+		 * @returns {string} The HTML id for the URL input field.
+		 */
+		function getTarget( $button ) {
+			$button = $( $button );
+
+			let target = $button.data( "target" );
+
+			// This is the implicit way to define which URL field to fill.
+			if ( ! target || target === "" ) {
+				target = $( $button ).attr( "id" ).replace( /_button$/, "" );
+			}
+
+			return target;
+		}
+
+		/**
+		 * Returns the hidden ID input element for this button.
+		 *
+		 * @param {Object} $button The image select button.
+		 * @returns {string} The HTML id for the ID input field.
+		 */
+		function getIdTarget( $button ) {
+			$button = $( $button );
+
+			return $button.data( "target-id" );
+		}
+
+		$( ".wpseo_image_upload_button" ).each( function( index, element ) {
+			const urlInputHtmlId = getTarget( element );
+			const idInputHtmlId = getIdTarget( element );
+
+			const $urlInput = $( "#" + urlInputHtmlId );
+			const $idInput = $( "#" + idInputHtmlId );
+
+			// eslint-disable-next-line
+			var wpseoCustomUploader = wp.media.frames.file_frame = wp.media( {
 				title: wpseoMediaL10n.choose_image,
 				button: { text: wpseoMediaL10n.choose_image },
-				multiple: false
-			});
+				multiple: false,
+			} );
 
-			wpseo_custom_uploader.on( 'select', function() {
-					var attachment = wpseo_custom_uploader.state().get( 'selection' ).first().toJSON();
-					$( '#' + wpseo_target_id ).val( attachment.url );
+			wpseoCustomUploader.on( "select", function() {
+					var attachment = wpseoCustomUploader.state().get( "selection" ).first().toJSON();
+
+					$urlInput.val( attachment.url );
+					$idInput.val( attachment.id );
 				}
 			);
 
-			$(element).click( function( e ) {
+			const $uploadImageButton = $( element );
+
+			$uploadImageButton.click( function( e ) {
 				e.preventDefault();
-				wpseo_custom_uploader.open();
+				wpseoCustomUploader.open();
 			} );
+
+			$uploadImageButton
+				.siblings( ".wpseo_image_remove_button" )
+				.on( "click", ( e ) => {
+				e.preventDefault();
+
+			$urlInput.val( "" );
+			$idInput.val( "" );
+		} );
 		} );
 	}
 );
